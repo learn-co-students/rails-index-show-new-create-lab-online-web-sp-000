fe385fadc07a9933ccf95fe1362074cbd8d10f4b
Ramon Chavez <rachavez1974@gmail.com>
2019-08-11 19:23:03 -0400

    Add create

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,8 @@
 class CouponsController < ApplicationController
+  before_action :set_coupon, only: [:show]
+
   def new
+    render 'new'
   end
 
   def index
@@ -7,8 +10,20 @@ class CouponsController < ApplicationController
   end
 
   def create
+    @coupon = Coupon.create(coupon_params)
+    redirect_to @coupon
   end
 
   def show
   end
+
+  private
+    def set_coupon
+      @coupon = Coupon.find(params[:id])
+    end
+
+    def coupon_params
+      params.require(:coupon).permit(:coupon_code, :store)
+    end
+
 end
