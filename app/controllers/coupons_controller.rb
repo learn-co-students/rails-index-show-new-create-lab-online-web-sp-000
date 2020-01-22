8ebaca72253f15f3718079a1b8a2487da78d981a
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-22 09:16:11 -0500

    add code to index and show views

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,13 +1,19 @@
 class CouponsController < ApplicationController
     def index
+        @coupons = Coupon.all
     end
 
     def show
+        @coupon = Coupon.find(params[:id])
     end
 
     def new
     end
 
     def create
+        @coupon = Coupon.new
+        @coupon.coupon_code = params[:coupon_code]
+        @coupon.store = params[:store]
+        @coupon.save
     end
 end
