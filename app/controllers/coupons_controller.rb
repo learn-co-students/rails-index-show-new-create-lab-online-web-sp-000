9fbef5f99ee20f08333e3deb0bf4f6a47c5e4b91
CoderJay06 <jaycru06@yahoo.com>
2020-05-20 01:54:38 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class CouponsController < ApplicationController
    def index 
       @coupons = Coupon.all 
@@ -6,15 +8,22 @@ class CouponsController < ApplicationController
    def new 
    end 
 
-   def create 
-      @coupon = Coupon.new
-      @coupon.coupon_code = params[:coupon_code]
-      @coupon.store = params[:store]
-      @coupon.save 
-      redirect_to coupon_path(@coupon)
-   end 
 
    def show 
       @coupon = Coupon.find(params[:id])
    end 
+
+   def create 
+      @coupon = Coupon.new
+      @coupon.coupon_code = coupon_params[:coupon_code]
+      @coupon.store = coupon_params[:store]
+      @coupon.save 
+      redirect_to coupon_path(@coupon)
+   end 
+
+   private 
+      # permit params
+      def coupon_params 
+         params.require(:coupon).permit(:coupon_code, :store)
+      end 
 end
