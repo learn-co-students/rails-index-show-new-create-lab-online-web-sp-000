1ec97af1314f06e94df289e01284e89abe394789
Alex Velazquez <alexvelazquez@Alexs-MacBook-Pro.local>
2020-07-08 16:34:47 -0700

    add index, show, new and create actions to coupons_controller.rb.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,23 @@
 class CouponsController < ApplicationController
 
+  def index 
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new 
+  end
+
+  def create 
+    @coupon = Coupon.create(
+      {
+        coupon_code: params[:coupon_code],
+        store: params[:store]
+      }
+    )
+  end
+
+
 end
