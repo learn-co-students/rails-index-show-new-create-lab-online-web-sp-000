eb7cc0bbf04a632a7ec561534c2f7c9d5b5c2886
Elliot Blanchard <git@elliotblanchard.com>
2020-07-24 17:32:05 -0400

    Done.

---
+++ app/models/cupon.rb
@@ -0,0 +1,3 @@
+class Cupon < ActiveRecord::Base
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200724211146) do
+
+  create_table "cupons", force: :cascade do |t|
+    t.string   "cupon_code", null: false
+    t.string   "store",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
