400592253e5459be0b1c12a7493993b62401048b
cantstopcoding <jonathan_koss@yahoo.com>
2020-07-27 18:20:47 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,8 +13,8 @@ class CouponsController < ApplicationController
     def create
         @coupon = Coupon.create(
             {
-              store: params[:store],
-              coupon_code: params[:coupon_code]
+              store: params[:coupon][:store],
+              coupon_code: params[:coupon][:coupon_code]
             }
         )
         redirect_to coupon_path(@coupon)
