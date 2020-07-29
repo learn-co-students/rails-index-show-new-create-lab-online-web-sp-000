86d1a7544655ecd72ec75bee819eeda3a42023a6
chienleow <chienleow1@gmail.com>
2020-07-29 09:55:39 -0400

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,16 @@
 class CouponsController < ApplicationController
-    
+    def index
+        @coupons = Coupon.all
+    end
+
+    def show
+        @coupon = Coupon.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+
+    end
 end
