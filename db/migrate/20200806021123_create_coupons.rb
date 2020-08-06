ac9031496cc66885e21440d46386561217bd4c6a
Taishawnk <Taishawnking1@yahoo.com>
2020-08-05 21:14:40 -0600

    Done.

--- db/migrate/20200806021123_create_coupons.rb
+++ db/migrate/20200806021123_create_coupons.rb
@@ -1,4 +1,4 @@
-class CreateCoupons < ActiveRecord::Migration[5.0]
+- Sctclass CreateCoupons < ActiveRecord::Migration[5.0]
   def change
     create_table :coupons do |t|
       t.string :coupon_code
