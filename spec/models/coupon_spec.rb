632373716004d71dc76ec866eed560f26a4f33d7
nllew <n.lewis.perc@gmail.com>
2021-01-18 14:00:51 -0600

    Done.

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Coupon do
+  before(:each) do
+    @coupon = Coupon.create(coupon_code: "FREEBIE", store: "Amazon")
+    @coupon = Coupon.create(coupon_code: "MOMMY", store: "Etsy")
+    @coupon = Coupon.create(coupon_code: "MONEY", store: "eBay")
+  end
+
+  it 'can be created' do
+    expect(@coupon).to be_valid
+  end
 end
